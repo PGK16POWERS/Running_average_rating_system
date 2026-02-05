c8f724c6c6d687bf9ec511f0b43a170348f52cbc
Liberia (Kaiberius) Kartier <136324051+PGK16POWERS@users.noreply.github.com>
2026-02-05 06:56:23 +0000

    basic func setup

---
+++ main.js
@@ -0,0 +1,21 @@
+const mainRate = document.querySelector("#main-rate");
+
+mainRate.textContent = 5;
+
+let count = 0
+
+/*
+
+    DESCRIPTION:
+
+    With this approach we only store the rate count and current average
+
+
+
+    THE FORMULA:
+
+    ((currentAverage * count) + newRate) / (count + 1);
+
+
+*/
+
